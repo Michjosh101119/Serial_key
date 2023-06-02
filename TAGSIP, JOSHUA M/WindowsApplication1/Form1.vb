779af5a95c9e27406b4accc1188c6b11260914fa
Michjosh101119 <135184307+Michjosh101119@users.noreply.github.com>
2023-06-02 06:37:28 -0700

    Add files via upload

---
+++ TAGSIP, JOSHUA M/WindowsApplication1/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        If My.Settings.AppStat = "Trial Version" Then
+
+        End If
+        If My.Settings.LoadCount < 0 Then
+            Form2.ShowDialog()
+
+        End If
+
+        Me.Text = My.Settings.AppStat & " : Count = > " &
+            My.Settings.LoadCount
+        My.Settings.LoadCount -= 1
+        My.Settings.Save()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("PUYOS, MICHELLE Y.")
+
+    End Sub
+End Class
---
+++ TAGSIP, JOSHUA M/WindowsApplication1/Form2.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form2
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub REGISTER_Click(sender As Object, e As EventArgs) Handles REGISTER.Click
+        '08032000
+        If My.Settings.SerialKey = TextBox1.Text Then
+            My.Settings.AppStat = "Full Version"
+            My.Settings.Save()
+            Close()
+        Else
+            MsgBox("Invalid Serial Key")
+
+        End If
+
+    End Sub
+End Class
